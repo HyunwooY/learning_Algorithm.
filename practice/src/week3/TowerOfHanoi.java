2780b95187b93e50d212f1dd5fbc27558cbb9871
yhw92 <yhw92@220.121.11.177>
2021-12-31 08:38:54 +0900

    .

---
+++ practice/src/week3/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+package week3;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+
+	}
+	public void hanoi(int num) {
+		if(num%2==0) {
+			
+		}else {
+			
+		}
+	}
+}
